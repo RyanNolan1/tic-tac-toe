e5806d01e00fb9c228fb5ab24179ab9176c0f073
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-09-13 21:31:13 +0100

    this will get a 3 x 3 grid displayed in the game container

--- display-controller.js
+++ display-controller.js
@@ -1,3 +1,15 @@
-const displayController = (function () {})();
+const displayController = (function () {
+  const gameContainer = document.getElementById("game-container");
+  function renderGrid(rows, cols) {
+    gameContainer.style.setProperty("--grid-rows", rows);
+    gameContainer.style.setProperty("--grid-cols", cols);
+    for (let i = 0; i < rows * cols; i++) {
+      let cell = document.createElement("div");
+      cell.innerText = i + 1;
+      gameContainer.appendChild(cell).className = "grid-item";
+    }
+  }
+  renderGrid(3, 3);
+})();
 
 export default displayController;
