3db43384b8ea8eee3d22ba5a7f6f5c7f014f41ad
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-09-15 07:05:49 +0100

    this will make the cells display either X or O on click

--- place-marker.js
+++ place-marker.js
@@ -1,11 +1,15 @@
-const placeMarker =  (function () {
-const gridItem = document.querySelectorAll(".grid-item");
+import theGameStructure from "./gameboard.js";
+
+const placeMarker = (function () {
+    const gridItem = document.querySelectorAll(".grid-item");
+    let whichMarker = 0;
     gridItem.forEach((element, index) => {
-      element.addEventListener("click", function () {
-        console.log("I've been clicked "+ index)
-        gridItem[index].style.backgroundColor = "red";
-      });
+        element.addEventListener("click", function () {
+        const X = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]  
+      gridItem[index].innerHTML = X[whichMarker];
+      whichMarker += 1;
     });
+  });
 })();
 
 export default placeMarker;
