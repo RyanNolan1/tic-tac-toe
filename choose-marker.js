1d1e5456f05379fe2c01e289cf0aa6b216d437ed
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-10-03 20:28:18 +0100

    this will make the chosen marker module return a different order of markers depending on which marker is chosen

--- choose-marker.js
+++ choose-marker.js
@@ -1,15 +1,16 @@
-const chooseMarker = (function () {
-  const chosenMarker = document.getElementsByName("choose-marker");
+const chosenMarker = (function () {
+  const pickMarker = document.getElementsByName("choose-marker");
   const continueButton = document.getElementById("continue-button");
 
-  continueButton.addEventListener('click', () =>  {
-    chosenMarker.forEach((element) => {
-        if (element.checked) {
-            console.log(element.value);
-        }
-        });
+  continueButton.addEventListener("click", () => {
+    pickMarker.forEach((element) => {
+      if (element.checked && element.value === "X") {
+        return ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+      } else if (element.checked && element.value === "O") {
+        return ["O", "X", "O", "X", "O", "X", "O", "X", "O"];
+      }
+    });
   });
 })();
 
-export default chooseMarker;
-
+export default chosenMarker;
