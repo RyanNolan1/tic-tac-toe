d2226f2c52e6c1cad5ccc4b6e542ce23e4b288d3
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-10-19 21:12:51 +0100

    this will add variables for the top, middle and bottom rows of the board

--- who-wins.js
+++ who-wins.js
@@ -8,7 +8,10 @@ const whoWins = (function () {
     element.addEventListener("click", function () {
         gameArray.splice(index, 1, element.innerHTML)
         console.log(gameArray);
-    });
+          const topRow = gameArray.slice(0, 3);
+          const middleRow = gameArray.slice(3, 6);
+          const bottomRow =  gameArray.slice(6, 9);
+      });
   });
 })();
 
