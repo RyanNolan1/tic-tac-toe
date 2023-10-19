2fb9640df358a30680bbfb128be7d46d1444c5fa
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-10-19 21:43:29 +0100

    this will add the variables for the columns and also console log if X or O wins on those columns

--- who-wins.js
+++ who-wins.js
@@ -10,20 +10,39 @@ const whoWins = (function () {
       const topRow = gameArray.slice(0, 3);
       const middleRow = gameArray.slice(3, 6);
       const bottomRow = gameArray.slice(6, 9);
+      const firstColumn = [gameArray[0], gameArray[3], gameArray[6]];
+      const middleColumn = [gameArray[1], gameArray[4], gameArray[7]];
+      const thirdColumn = [gameArray[2], gameArray[5], gameArray[8]];
       const diagonalDown = [gameArray[0], gameArray[4], gameArray[8]];
       const diagonalUp = [gameArray[6], gameArray[4], gameArray[2]];
 
       const isItX = (marker) => marker === "X";
-      if (topRow.every(isItX) || middleRow.every(isItX) || bottomRow.every(isItX) || diagonalDown.every(isItX) || diagonalUp.every(isItX)) {
+      if (
+        topRow.every(isItX) ||
+        middleRow.every(isItX) ||
+        bottomRow.every(isItX) ||
+        firstColumn.every(isItX) ||
+        middleColumn.every(isItX) ||
+        thirdColumn.every(isItX) ||
+        diagonalDown.every(isItX) ||
+        diagonalUp.every(isItX)
+      ) {
         console.log("X Wins!");
       }
 
       const isItO = (marker) => marker === "O";
-      if (topRow.every(isItO) || middleRow.every(isItO) || bottomRow.every(isItO) || diagonalDown.every(isItO) || diagonalUp.every(isItO)) {
+      if (
+        topRow.every(isItO) ||
+        middleRow.every(isItO) ||
+        bottomRow.every(isItO) ||
+        firstColumn.every(isItO) ||
+        middleColumn.every(isItO) ||
+        thirdColumn.every(isItO) ||
+        diagonalDown.every(isItO) ||
+        diagonalUp.every(isItO)
+      ) {
         console.log("O Wins!");
       }
-
-
     });
   });
 })();
