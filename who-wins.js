f2dde11341488e043fea4ab834fdcd924f99f88c
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-10-19 21:35:10 +0100

    this will console log when either X or O wins

--- who-wins.js
+++ who-wins.js
@@ -6,15 +6,25 @@ const whoWins = (function () {
 
   gridItem.forEach((element, index) => {
     element.addEventListener("click", function () {
-        gameArray.splice(index, 1, element.innerHTML)
-        // console.log(gameArray);
-          const topRow = gameArray.slice(0, 3);
-          const middleRow = gameArray.slice(3, 6);
-          const bottomRow =  gameArray.slice(6, 9);
-          const diagonalDown = [gameArray[0], gameArray[4], gameArray[8]];
-          const diagonalUp = [gameArray[6], gameArray[4], gameArray[2]];
-          console.log(diagonalUp)
-      });
+      gameArray.splice(index, 1, element.innerHTML);
+      const topRow = gameArray.slice(0, 3);
+      const middleRow = gameArray.slice(3, 6);
+      const bottomRow = gameArray.slice(6, 9);
+      const diagonalDown = [gameArray[0], gameArray[4], gameArray[8]];
+      const diagonalUp = [gameArray[6], gameArray[4], gameArray[2]];
+
+      const isItX = (marker) => marker === "X";
+      if (topRow.every(isItX) || middleRow.every(isItX) || bottomRow.every(isItX) || diagonalDown.every(isItX) || diagonalUp.every(isItX)) {
+        console.log("X Wins!");
+      }
+
+      const isItO = (marker) => marker === "O";
+      if (topRow.every(isItO) || middleRow.every(isItO) || bottomRow.every(isItO) || diagonalDown.every(isItO) || diagonalUp.every(isItO)) {
+        console.log("O Wins!");
+      }
+
+
+    });
   });
 })();
 
