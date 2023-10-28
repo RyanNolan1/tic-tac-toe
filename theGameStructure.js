0603c150ae601fff5d60b3d10ea7c587d5be20d1
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-10-28 21:21:04 +0100

    this will log the entered players names into the gameboard object

--- theGameStructure.js
+++ theGameStructure.js
@@ -1,12 +1,31 @@
 const theGameStructure = (function () {
-  const gameBoard = {
-    rows: 3,
-    columns: 3,
-    gameArray: ["", "", "", "", "", "", "", "", ""],
+  const continueButton = document.getElementById("continue-button");
+  const playerOneName = document.getElementById("player-1-name");
+  const playerTwoName = document.getElementById("player-2-name");
+
+  const playerFactory = (name) => {
+    console.log(name);
+    return name;
   };
+  
+  const gameBoard = {
+   playerOne: "",
+   playerTwo: "",
+   rows: 3,
+   columns: 3,
+   gameArray: ["", "", "", "", "", "", "", "", ""],
+ };
+
+  function createPlayer() {
+    gameBoard.playerOne = playerFactory(playerOneName.value);
+    gameBoard.playerTwo = playerFactory(playerTwoName.value);
+    console.log(gameBoard);
+  }
+
+  continueButton.addEventListener("click", createPlayer);
+
 
   return gameBoard;
-
 })();
 
 export default theGameStructure;
